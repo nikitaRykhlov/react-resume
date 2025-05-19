ed3bd3ab2b9175a0fbf4d435866e116025a85c1e
nikitaRykhlov <nikita_rykhlov@outlook.com>
2025-05-19 20:47:27 +0400

    fix navigation responsibility

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -21,104 +21,104 @@ const navItems = [['About', 'about'], ['Experience', 'experience'], ['Education'
 
 function Navigation({parentToChild, modeChange}: any) {
 
-    const {mode} = parentToChild;
+  const {mode} = parentToChild;
 
-    const [mobileOpen, setMobileOpen] = useState<boolean>(false);
-    const [scrolled, setScrolled] = useState<boolean>(false);
+  const [mobileOpen, setMobileOpen] = useState<boolean>(false);
+  const [scrolled, setScrolled] = useState<boolean>(false);
 
-    const handleDrawerToggle = () => {
-        setMobileOpen((prevState) => !prevState);
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const navbar = document.getElementById("navigation");
+      if (navbar) {
+        const scrolled = window.scrollY > navbar.clientHeight;
+        setScrolled(scrolled);
+      }
     };
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const navbar = document.getElementById("navigation");
-            if (navbar) {
-                const scrolled = window.scrollY > navbar.clientHeight;
-                setScrolled(scrolled);
-            }
-        };
+    window.addEventListener('scroll', handleScroll);
 
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    const scrollToSection = (section: string) => {
-        console.log(section)
-        const expertiseElement = document.getElementById(section);
-        if (expertiseElement) {
-            expertiseElement.scrollIntoView({behavior: 'smooth'});
-            console.log('Scrolling to:', expertiseElement);  // Debugging: Ensure the element is found
-        } else {
-            console.error('Element with id "expertise" not found');  // Debugging: Log error if element is not found
-        }
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
     };
+  }, []);
 
-    const drawer = (
-        <Box className="navigation-bar-responsive" onClick={handleDrawerToggle} sx={{textAlign: 'center'}}>
-            <p className="mobile-menu-top"><ListIcon/>Menu</p>
-            <Divider/>
-            <List>
-                {navItems.map((item) => (
-                    <ListItem key={item[0]} disablePadding>
-                        <ListItemButton sx={{textAlign: 'center'}} onClick={() => scrollToSection(item[1])}>
-                            <ListItemText primary={item[0]}/>
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-        </Box>
-    );
+  const scrollToSection = (section: string) => {
+    console.log(section)
+    const expertiseElement = document.getElementById(section);
+    if (expertiseElement) {
+      expertiseElement.scrollIntoView({ behavior: 'smooth' });
+      console.log('Scrolling to:', expertiseElement);  // Debugging: Ensure the element is found
+    } else {
+      console.error('Element with id "expertise" not found');  // Debugging: Log error if element is not found
+    }
+  };
 
-    return (
-        <Box sx={{display: 'flex'}}>
-            <CssBaseline/>
-            <AppBar component="nav" id="navigation" className={`navbar-fixed-top${scrolled ? ' scrolled' : ''}`}>
-                <Toolbar className='navigation-bar'>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                        onClick={handleDrawerToggle}
-                        sx={{mr: 2, display: {lg: 'none'}}}
-                    >
-                        <MenuIcon/>
-                    </IconButton>
-                    {mode === 'dark' ? (
-                        <LightModeIcon onClick={() => modeChange()}/>
-                    ) : (
-                        <DarkModeIcon onClick={() => modeChange()}/>
-                    )}
-                    <Box sx={{display: {md: 'none', lg: 'block'}}}>
-                        {navItems.map((item) => (
-                            <Button key={item[0]} onClick={() => scrollToSection(item[1])} sx={{color: '#fff'}}>
-                                {item[0]}
-                            </Button>
-                        ))}
-                    </Box>
-                </Toolbar>
-            </AppBar>
-            <nav>
-                <Drawer
-                    variant="temporary"
-                    open={mobileOpen}
-                    onClose={handleDrawerToggle}
-                    ModalProps={{
-                        keepMounted: true, // Better open performance on mobile.
-                    }}
-                    sx={{
-                        display: {lg: 'block', xl: 'none'},
-                        '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},
-                    }}
-                >
-                    {drawer}
-                </Drawer>
-            </nav>
-        </Box>
-    );
+  const drawer = (
+    <Box className="navigation-bar-responsive" onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <p className="mobile-menu-top"><ListIcon/>Menu</p>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item[0]} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }} onClick={() => scrollToSection(item[1])}>
+              <ListItemText primary={item[0]} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav" id="navigation" className={`navbar-fixed-top${scrolled ? ' scrolled' : ''}`}>
+        <Toolbar className='navigation-bar'>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { lg: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          {mode === 'dark' ? (
+            <LightModeIcon onClick={() => modeChange()}/>
+          ) : (
+            <DarkModeIcon onClick={() => modeChange()}/>
+          )}
+          <Box sx={{ display: { xs: 'none', lg: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item[0]} onClick={() => scrollToSection(item[1])} sx={{ color: '#fff' }}>
+                {item[0]}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { md: 'block', lg: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </Box>
+  );
 }
 
 export default Navigation;
